63d62279ce7053f5e7025589edab49dea5988bc6
PaketniyMaster <krasin05@mail.ru>
2025-03-17 18:29:01 +0500

    CORS EMAE 11

--- app/main.py
+++ app/main.py
@@ -29,7 +29,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        ["*"]
+        "*"
         # "http://localhost",
         # "http://localhost:5173",
         # "http://127.0.0.1:8000",
