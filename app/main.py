91297c0b3ece3638c02bd4f2d022f74a4abf8b22
PaketniyMaster <krasin05@mail.ru>
2025-03-17 10:27:00 +0500

    CORS EMAE 2

--- app/main.py
+++ app/main.py
@@ -44,7 +44,7 @@ app.include_router(search.router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*", "http://127.0.0.1:8000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
